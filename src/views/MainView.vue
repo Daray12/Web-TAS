96742a28752c5d65423c8f5a8899ece63a57ec36
GunMetalBlack <powerhousegames206@gmail.com>
2023-01-30 09:12:32 -0500

    Added Server call Needs to be passed by value

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,5 +1,17 @@
 <script setup>
+import { onMounted } from "vue";
 import Main from "../components/Main.vue";
+import axios from "axios";
+
+
+onMounted(()=>{
+  axios.get('https://my-json-server.typicode.com/Daray12/Web-TAS/db').then((response)=> {
+    //studentData = response.data;
+    //Add a way to send this to the main.Vue file
+  }).catch((error)=>{
+    console.log(error)
+  })
+})
 </script>
 
 <template>
